11a480563177d93d7631320a470cf116da3d9bd5
vattcarter7 <vattcarter@gmail.com>
2020-03-03 21:00:19 +0700

    add searchBook and findBookByGoogleId functions

--- src/helpers/bookHelper.js
+++ src/helpers/bookHelper.js
@@ -1,8 +1,36 @@
 const axios = require('axios');
+const { map, groupBy, pathOr } = require('ramda');
 
+const db = require('../db');
 const ErrorResponse = require('./errorResponse');
 const asyncHandler = require('../middlewares/async');
 
-exports.findBookByGoogleId = asyncHandler(async(req, res, next) => {
-  const response = axios.get('')
-})
+exports.searchBook = query =>
+  asyncHandler(
+    asyncHandler(async (req, res, next) => {
+      const url = `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(
+        query
+      )}`;
+      try {
+        const result = await axios(url);
+        const items = pathOr([], ['data', 'items', result]);
+        const books = map(book => ({ id: book.id, ...book.volumeInfo }), items);
+        return books;
+      } catch (err) {
+        console.log(err);
+        throw err;
+      }
+    })
+  );
+
+exports.findBookByGoogleId = asyncHandler(async (req, res, next) => {
+  try {
+    const url = `https://www.googleapis.com/books/v1/volumes/${googleBookId}`;
+    const result = await axios(url);
+    const book = pathOr({}, ['data', result]);
+    return { ...book, ...book.volumeInfo };
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+});
